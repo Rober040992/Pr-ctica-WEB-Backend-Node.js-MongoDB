dda218fed4ce6dfb7b19147f72ab1e826787c645
RoberGF <rgfrasta@gmail.com>
2025-01-02 19:59:45 +0100

    creating api routes and apiProductController with same filters than website homeController

--- nodepop/app.js
+++ nodepop/app.js
@@ -11,22 +11,23 @@ import * as sessionManager from './config/sessionManager.js'
 import upload from './config/uploadConfig.js';
 import i18n from './config/i18nConfig.js';
 import * as langController from './controllers/langController.js'
+//controllers del API:
+import * as apiPorductController from './controllers/api/apiProductController.js'
+
 //en este file app.js configuramos la aplicación de Express, definimos el uso de los middlewares y las rutas
 
-await connectMongoose()
+await connectMongoose() // esperamos a la conexion de la DB
 console.log('Conectado a MongoDB')
 
-const app = express()
+const app = express() //inicializamos express
 
-//aqui conectamos con header y renderizamos la app
-app.locals.appName = 'nodepop'
+app.locals.appName = 'nodepop'//aqui conectamos con header y renderizamos la app
 
 // view engine setup
 app.set('views', join(import.meta.dirname, 'views')) //MOTOR DE PLANTILLS
 app.set('view engine', 'ejs')
 
 // middlewares
-
 // morgan logger for http requests logs
 app.use(logger('dev'))
 // transforms json objects into js objects
@@ -37,6 +38,10 @@ app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
 // set the folder where statis resources will be served
 app.use(express.static(join(import.meta.dirname, 'public')))
+
+// API ROUTES
+app.get('/api/products', apiPorductController.apiProductGetList)
+
 
 // WEBSITE ROUTES
 
---
+++ nodepop/controllers/api/apiProductController.js
@@ -0,0 +1,32 @@
+import Product from "../../models/Product.js"
+// iportamos el modelo del producto
+export async function apiProductGetList(req, res, next){
+    try{
+        const limit = parseInt(req.query.limit) // || 5 para que aparezcan directamente un max de 5 productos
+        const skip = parseInt(req.query.skip) || 0 //ejm: http://localhost:5555/api/products/?limit=3&skip=3
+        const filter = { }
+        if (req.query.minPrice || req.query.maxPrice) { //verificamos que existan en la query
+            filter.price = {}; //iniciamos campo price
+            if (req.query.minPrice) {
+                filter.price.$gte = req.query.minPrice;
+            }
+            if (req.query.maxPrice) {
+                filter.price.$lte = req.query.maxPrice;
+            }
+        }
+        
+        if(req.query.name){
+            filter.name = new RegExp(req.query.name, 'i')// Insensible a mayúsculas
+        }
+
+        const products = await Product.list(filter, limit, skip)
+        const totalProducts = await Product.countDocuments(filter)
+        res.locals.products = products.length > 0 ? products : [];
+        res.locals.totalProducts = totalProducts;
+        res.json({ results: products})
+        // devolvemos una lista con todos los protuctos
+    } catch (error){
+        next(error)
+    }
+
+}
